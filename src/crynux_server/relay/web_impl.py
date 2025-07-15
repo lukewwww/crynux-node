bf42509ef281989f869b0da3807604ae11e1f1ed
iwehf <henry.lee@crynux.ai>
2025-07-15 20:38:21 +0800

    bug fix: send staking in node join request correctly

--- src/crynux_server/relay/web_impl.py
+++ src/crynux_server/relay/web_impl.py
@@ -284,14 +284,13 @@ class WebRelay(Relay):
     async def node_join(
         self, gpu_name: str, gpu_vram: int, model_ids: List[str], version: str, staking_amount: int
     ):
-        staking = str(Web3.to_wei(staking_amount, "ether"))
         input = {
             "address": self.node_address,
             "gpu_name": gpu_name,
             "gpu_vram": gpu_vram,
             "model_ids": model_ids,
             "version": version,
-            "staking": staking,
+            "staking": str(staking_amount),
         }
         timestamp, signature = self.signer.sign(input)
         resp = await self.client.post(
@@ -301,7 +300,7 @@ class WebRelay(Relay):
                 "gpu_vram": gpu_vram,
                 "model_ids": model_ids,
                 "version": version,
-                "staking": staking,
+                "staking": str(staking_amount),
                 "timestamp": timestamp,
                 "signature": signature,
             },
